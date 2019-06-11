719689f07a477c2165d96e69f28c76ce0821bd2b
Marius <mariusmichel@live.de>
2019-06-11 18:27:34 +0200

    palindrom works

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -3,7 +3,31 @@ package ueb01;
 class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+		CharStackImpl cs = new CharStackImpl();
 
-		return false;
+		Boolean pal = true;
+
+		s = s.toLowerCase();
+		s = s.replaceAll(" ", "");
+		char [] charArray = s.toCharArray();
+
+
+		if (charArray.length == 1){
+			return pal;
+		} else {
+			for (char c: charArray) {
+				cs.push(c);
+			}
+
+			int j = cs.size() - 1;
+			for (int i = 0; i <= cs.size()/2; i++){
+				if(charArray[i] != charArray[j]){
+					pal = false;
+				} else {
+					j--;
+				}
+			}
+		}
+		return pal;
 	}
 }
--- src/test/java/ueb01/PalindromTest.java
+++ src/test/java/ueb01/PalindromTest.java
@@ -1,37 +1,46 @@
-//package ueb01;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class PalindromTest {
-//	@Test
-//	void testPalindrom() {
-//		String[] xs = {
-//				"a",
-//				"aa",
-//				"Ana",
-//				"Anna",
-//				"Regallager",
-//				"Ein Neger mit Gazelle zagt im Regen nie",
-//				"Eine güldne gute Tugend Lüge nie"
-//		};
-//
-//		for (String s : xs) {
-//			System.err.println(s);
-//			assertTrue(Palindrom.istPalindrom(s));
-//		}
-//
-//		String[] zs = {
-//				"hansdampf",
-//				"peter pan",
-//				"quatsch mit sosse",
-//				"ab"
-//		};
-//
-//		for (String s : zs) {
-//			System.err.println(s);
-//			assertFalse(Palindrom.istPalindrom(s));
-//		}
-//	}
-//}
+package ueb01;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromTest {
+
+
+	@Test
+	void testPalindrom() {
+
+		String[] xs = {
+				"a",
+				"aa",
+				"Ana",
+				"Anna",
+				"Regallager",
+				"Ein Neger mit Gazelle zagt im Regen nie",
+				"Eine g\u00fcldne gute Tugend L\u00fcge nie"
+		};
+
+
+		for (String s : xs) {
+			System.err.println(s);
+//			System.out.println(Palindrom.istPalindrom(s));
+			assertTrue(Palindrom.istPalindrom(s));
+		}
+
+		String[] zs = {
+				"hansdampf",
+				"peter pan",
+				"quatsch mit sosse",
+				"ab"
+		};
+
+		for (String s : zs) {
+			System.err.println(s);
+//			System.out.println(Palindrom.istPalindrom(s));
+			assertFalse(Palindrom.istPalindrom(s));
+		}
+
+
+	}
+}
